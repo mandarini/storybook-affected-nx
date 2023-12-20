ef4d4ff98b4d6dd1dd0a88fc9592e7ed6d5380ad
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-12-20 19:08:13 +0200

    storyubook

---
+++ four/src/lib/four/four.component.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { FourComponent } from './four.component';
+
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const meta: Meta<FourComponent> = {
+  component: FourComponent,
+  title: 'FourComponent',
+};
+export default meta;
+type Story = StoryObj<FourComponent>;
+
+export const Primary: Story = {
+  args: {},
+};
+
+export const Heading: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/four works!/gi)).toBeTruthy();
+  },
+};
