bd3a3b5cd8033decd07d316323f45c75c5c1e151
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-12-20 19:11:51 +0200

    nx json again

--- four/src/lib/four/four.component.stories.ts
+++ four/src/lib/four/four.component.stories.ts
@@ -15,7 +15,7 @@ export const Primary: Story = {
   args: {},
 };
 
-export const Hello: Story = {
+export const asdfasf: Story = {
   args: {},
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
