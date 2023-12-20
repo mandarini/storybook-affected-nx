145fcc94e7874b1d9f29cda47e4b889a503ffaa2
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-12-20 18:32:57 +0200

    importing

--- three/src/lib/three/three.component.ts
+++ three/src/lib/three/three.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OneComponent } from '@angularstorybook/one';
 
 @Component({
   selector: 'angularstorybook-three',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, OneComponent],
   templateUrl: './three.component.html',
   styleUrl: './three.component.css',
 })
--- two/src/lib/twp/twp.component.ts
+++ two/src/lib/twp/twp.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OneComponent } from '@angularstorybook/one';
 
 @Component({
   selector: 'angularstorybook-twp',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, OneComponent],
   templateUrl: './twp.component.html',
   styleUrl: './twp.component.css',
 })
